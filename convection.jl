cbd756b7ba2c0b67f490bdb1a76c5481993cacaa
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-07-07 17:04:28 +0200

    update

--- convection.jl
+++ convection.jl
@@ -7,7 +7,7 @@ using Printf, LinearAlgebra
 model = PS_Setup(:cpu, Float64, 2)
 environment!(model)
 
-include("/home/albert/Desktop/JustPIC.jl/src/JustPIC.jl")
+include("C:\\Users\\albert\\Desktop\\JustPIC.jl\\src\\JustPIC.jl")
 using .JustPIC
 
 @parallel_indices (i, j) function init_T!(T, ΔT, w, ix, iy, dx, dy, lx, ly)
@@ -20,18 +20,30 @@ end
     return
 end
 
-nx=64
-ny=64
+compute_dt(S::StokesArrays, di::NTuple{2,T}, dt_diff) where T = compute_dt(S.V, di, dt_diff)
+compute_dt(V::Velocity, di::NTuple{2,T}, dt_diff) where T = compute_dt(V.Vx, V.Vy, di[1], di[2], dt_diff)
+
+function compute_dt(Vx, Vy, dx, dy, dt_diff)
+    dt_adv = min(dx/maximum(abs.(Vx)), dy/maximum(abs.(Vy))) / 2.1
+    return  min(dt_diff, dt_adv)
+end
+
+@parallel function add_dTdt(T, ΔT)
+    @inn(T) = @inn(T) + @inn(ΔT)
+    return nothing
+end
+        
+nx=287
+ny=95
 lx=3e0
 ly=1e0
 
 function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
-    dt = 1.0 # physical time step
-
+   
     # Physical domain
     ni = (nx, ny)
     li = (lx, ly)  # domain length in x- and y-
-    di = @. li / ni # grid step in x- and -y
+    di = @. li / (ni-1) # grid step in x- and -y
     max_li = max(li...)
     xci, xvi= lazy_grid(di, li; origin=(-lx/2, -ly/2)) # nodes at the center and vertices of the cells
 
@@ -49,7 +61,8 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     w         = 1e-2*ly            # initial perturbation standard deviation, m
     ρ0gα      = Ra*η0*κ/ΔT/ly^3    # thermal expansion
     dη_dT     = 1e-10/ΔT           # viscosity's temperature dependence
-    # Numerics
+    dt_diff   = 1.0/4.1*min(di...)^2/κ      # diffusive CFL timestep limiter
+    dt        = dt_diff # physical time step
 
     # Thermal diffusion ----------------------------------
     # general thermal arrays
@@ -63,8 +76,8 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     K = ρCp.*κ
     thermal_parameters = ThermalParameters(K, ρCp)
 
-    pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li)
-    thermal_bc = (flux_x=false, flux_y=true)
+    pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li, CFL= 0.1 / √2)
+    thermal_bc = (flux_x=true, flux_y=false)
 
     # @parallel (1:nx, 1:ny) init_T!(thermal.T, ΔT, w, 1:nx, 1:ny, di..., lx, ly)
 
@@ -74,7 +87,7 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     
     @parallel assign!(thermal.Told, thermal.T)
     # ----------------------------------------------------
-    
+
     # Stokes ---------------------------------------------
     ## Allocate arrays needed for every Stokes problem
     # general stokes arrays
@@ -95,17 +108,16 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     nxcell = 4 # initial number of particles per cell
     max_xcell = 8 # max number of particles per cell
     min_xcell = 2 # min number of particles per cell
-    particles = init_particles(nxcell, max_xcell, min_xcell, xci..., di..., ni...)
+    particles = init_particles(nxcell, max_xcell, min_xcell, xvi..., di..., ni...)
     pT = similar(particles.coords[1])
+    grid2particle!(pT, xvi, thermal.T, particles.coords)
+    particle_args = (pT,)
     # ----------------------------------------------------
 
-
-
+    # Physical time loop
     t = 0.0
     it = 0
-    nt = 10
-    
-    # Physical time loop
+    nt = 5
     local iters
     while it < nt
         # Stokes solver
@@ -118,8 +130,11 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
             freeslip, 
             fy, 
             η; 
-            iterMax=10e3
+            iterMax=10_000
         )
+
+        dt = compute_dt(stokes, di, dt_diff)
+        pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li, CFL= 0.1 / √2)
 
         # Thermal solver
         iters = solve!(
@@ -130,7 +145,7 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
             ni,
             di,
             dt;
-            iterMax=10e3,
+            iterMax=10_000,
             nout=1,
             verbose=false,
         )
@@ -141,21 +156,22 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
         fy = ρ .* g.* thermal.T
 
         # Particles advection
-        grid2particle!(pT, xci, thermal.ΔT, particles.coords)
-
-        gathering!(T, pT, grid, particles.coords, particles.upper_buffer, particles.lower_buffer)
+        grid2particle!(pT, xvi, thermal.ΔT, particles.coords)
 
         # advect particles in space
-        advection_RK2!(particles, V, grid, dxi, dt, α)
+        advection_RK2!(particles, (stokes.V.Vx, stokes.V.Vy), xci, di, dt, 0.5)
 
         # advect particles in memory
-        shuffle_particles!(particles, grid, dxi, nxi, args)
+        shuffle_particles!(particles, xvi, di, ni, particle_args)
 
-        check_injection(particles.inject) && (
-            inject_particles!(particles, grid, nxi, dxi);
-            grid2particle!(pT, grid, T, particles.coords)
-        )
+        particle2node!(thermal.ΔT, pT, xvi, particles)
         
+        @parallel add_dTdt(thermal.T, thermal.ΔT)
+        
+        check_injection(particles.inject) && inject_particles!(particles, xvi, ni, di)
+
+        # gathering!(thermal.ΔT, pT, xci, particles.coords, particles.upper_buffer, particles.lower_buffer)
+
         it += 1
         t += dt
     end
