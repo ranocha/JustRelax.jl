a5b2fa0eb745fa97c3450913d3a814134f4c464f
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-07-07 18:14:58 +0200

    update

--- convection.jl
+++ convection.jl
@@ -93,12 +93,12 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     # general stokes arrays
     stokes = StokesArrays(ni, Viscous)
     # general numerical coeffs for PT stokes
-    pt_stokes = PTStokesCoeffs(ni, di)
+    pt_stokes = PTStokesCoeffs(ni, di; ϵ = 1e-4)
 
     ## Setup-specific parameters and fields
     η = @zeros(ni...) # viscosity field
     @parallel viscosity!(η, thermal.T, ΔT, dη_dT)
-    fy = ρ0gα.* thermal.T
+    fy = -ρ0gα.* thermal.T
 
     ## Boundary conditions
     freeslip = (freeslip_x=true, freeslip_y=true)
@@ -117,7 +117,7 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
     # Physical time loop
     t = 0.0
     it = 0
-    nt = 5
+    nt = 10
     local iters
     while it < nt
         # Stokes solver
@@ -134,7 +134,7 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
         )
 
         dt = compute_dt(stokes, di, dt_diff)
-        pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li, CFL= 0.1 / √2)
+        pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li; ϵ = 1e-4, CFL= 0.1 / √2)
 
         # Thermal solver
         iters = solve!(
@@ -146,34 +146,39 @@ function thermal_convection2D(; nx=64, ny=64, lx=3e0, ly=1e0)
             di,
             dt;
             iterMax=10_000,
-            nout=1,
+            nout=10,
             verbose=false,
         )
 
-        # Update viscosity
-        @parallel viscosity!(η, thermal.T, ΔT, dη_dT)
-        # Update buoyancy
-        fy = ρ .* g.* thermal.T
-
         # Particles advection
-        grid2particle!(pT, xvi, thermal.ΔT, particles.coords)
+        grid2particle!(pT, xci, thermal.ΔT, particles.coords)
 
         # advect particles in space
-        advection_RK2!(particles, (stokes.V.Vx, stokes.V.Vy), xci, di, dt, 0.5)
+        advection_RK2!(particles, (stokes.V.Vx, stokes.V.Vy), xvi, di, dt, 0.5)
 
         # advect particles in memory
         shuffle_particles!(particles, xvi, di, ni, particle_args)
 
-        particle2node!(thermal.ΔT, pT, xvi, particles)
+        particle2grid!(thermal.ΔT, pT, xvi, particles)
         
         @parallel add_dTdt(thermal.T, thermal.ΔT)
         
-        check_injection(particles.inject) && inject_particles!(particles, xvi, ni, di)
-
-        # gathering!(thermal.ΔT, pT, xci, particles.coords, particles.upper_buffer, particles.lower_buffer)
+        check_injection(particles.inject) && inject_particles!(particles, xci, ni, di)
+         
+        # Update viscosity
+        @parallel viscosity!(η, thermal.T, ΔT, dη_dT)
+        thermal.T[:,1  ] .=  ΔT/2.0
+        thermal.T[:,end] .= -ΔT/2.0
+        
+        # Update buoyancy
+        fy = -ρ0gα.* thermal.T
 
         it += 1
         t += dt
+
+        # @show extrema(stokes.V.Vy)
+        # heatmap(xci[1], xci[2], thermal.T , colormap=:vik, colorrange=(-0.5, 0.5))
+
     end
 
     return (ni=ni, xci=xci, li=li, di=di), thermal, iters
