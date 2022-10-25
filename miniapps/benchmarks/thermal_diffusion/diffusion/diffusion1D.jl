54171a3250e5d7096844d99e78488b475d5b402f
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-10-25 16:11:29 +0200

    fix 1D benchmark

--- miniapps/benchmarks/thermal_diffusion/diffusion/diffusion1D.jl
+++ miniapps/benchmarks/thermal_diffusion/diffusion/diffusion1D.jl
@@ -1,62 +1,61 @@
-@parallel_indices (i) function init_T!(T, z)
+@parallel_indices (i) function init_T!(T)
     if i == length(T)
         T[i] = 300.0
     elseif i == 1
         T[i] = 3500.0
     else
-        T[i] = z[i] * (1900.0 - 1600.0) / minimum(z) + 1600.0
+        T[i] = 1900.0
     end
     return nothing
 end
 
-function diffusion_1D(; nx=128, lx=100e3, ρ=3.3e3, Cp=1.2e3, K=3.0)
-    kyr = 1e3 * 3600 * 24 * 365.25
+function diffusion_1D(; nx=128, lx=100e3, ρ0=3.3e3, Cp0=1.2e3, K0=3.0)
     Myr = 1e6 * 3600 * 24 * 365.25
     ttot = 10 * Myr # total simulation time
-    dt = 50 * kyr # physical time step
 
     # Physical domain
     ni = (nx,)
     li = (lx,)  # domain length in x- and y-
-    di = @. li / ni # grid step in x- and -y
-    xci, = lazy_grid(di, li; origin=(-lx,)) # nodes at the center and vertices of the cells
+    di = @. li / (ni-1) # grid step in x- and -y
+    xci, xvi = lazy_grid(di, li; origin=(-lx,)) # nodes at the center and vertices of the cells
 
     ## Allocate arrays needed for every Thermal Diffusion
     # general thermal arrays
     thermal = ThermalArrays(ni)
 
     # physical parameters
-    ρ = @fill(ρ, ni...)
-    Cp = @fill(Cp, ni...)
-    K = @fill(K, ni...)
+    κ = K0/(ρ0*Cp0)
+    ρ = @fill(ρ0, ni...)
+    Cp = @fill(Cp0, ni...)
+    K = @fill(K0, ni...)
     ρCp = @. Cp * ρ
     thermal_parameters = ThermalParameters(K, ρCp)
+    thermal_bc = (flux_y=false, )
 
-    pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li; CFL=0.8 / √3)
-
-    @parallel (1:nx) init_T!(thermal.T, xci[1])
+    thermal.T .= 1900.0
+    thermal.T[end] = 0.0
+    thermal.T[1] = 3500.0
     @parallel assign!(thermal.Told, thermal.T)
+
+    dt = 0.5 / 4.1 * min(di...)^2 / κ
 
     t = 0.0
     it = 0
     nt = Int(ceil(ttot / dt))
     # Physical time loop
-    local iters
+    scatter(thermal.T, xvi[1])
     while it < nt
-        iters = solve!(
+        solve!(
             thermal,
-            pt_thermal,
             thermal_parameters,
-            ni,
+            thermal_bc,
             di,
-            dt;
-            iterMax=10e3,
-            nout=1,
-            verbose=false,
+            dt
         )
         it += 1
         t += dt
+        scatter!(thermal.T, xvi[1])
     end
 
-    return (ni=ni, xci=xci, li=li, di=di), thermal, iters
+    return (ni=ni, xci=xci, xvi=xvi, li=li, di=di), thermal.T
 end
