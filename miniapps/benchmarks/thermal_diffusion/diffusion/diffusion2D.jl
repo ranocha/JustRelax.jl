306b46ff0378d0917f02223372aaf263d6e09afa
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-10-25 15:36:22 +0200

    fix 2D benchmark

--- miniapps/benchmarks/thermal_diffusion/diffusion/diffusion2D.jl
+++ miniapps/benchmarks/thermal_diffusion/diffusion/diffusion2D.jl
@@ -4,61 +4,54 @@
     elseif z[j] == minimum(z)
         T[i, j] = 3500.0
     else
-        T[i, j] = z[j] * (1900.0 - 1600.0) / minimum(z) + 1600.0
+        T[i, j] = 1900.0
     end
     return nothing
 end
 
-function diffusion_2D(; nx=32, ny=32, lx=100e3, ly=100e3, ρ=3.3e3, Cp=1.2e3, K=3.0)
-    kyr = 1e3 * 3600 * 24 * 365.25
+function diffusion_2D(; nx=32, ny=32, lx=100e3, ly=100e3, ρ0=3.3e3, Cp0=1.2e3, K0=3.0)
     Myr = 1e6 * 3600 * 24 * 365.25
     ttot = 10 * Myr # total simulation time
-    dt = 50 * kyr # physical time step
 
     # Physical domain
     ni = (nx, ny)
     li = (lx, ly)  # domain length in x- and y-
-    di = @. li / ni # grid step in x- and -y
-    xci, = lazy_grid(di, li; origin=(0, -ly)) # nodes at the center and vertices of the cells
+    di = @. li / (ni-1) # grid step in x- and -y
+    xci, xvi = lazy_grid(di, li; origin=(0, -ly)) # nodes at the center and vertices of the cells
 
     ## Allocate arrays needed for every Thermal Diffusion
     # general thermal arrays
     thermal = ThermalArrays(ni)
 
     # physical parameters
-    ρ = @fill(ρ, ni...)
-    Cp = @fill(Cp, ni...)
-    K = @fill(K, ni...)
+    κ = K0/(ρ0*Cp0)
+    ρ = @fill(ρ0, ni...)
+    Cp = @fill(Cp0, ni...)
+    K = @fill(K0, ni...)
     ρCp = @. Cp * ρ
     thermal_parameters = ThermalParameters(K, ρCp)
+    thermal_bc = (flux_x=true, flux_y=false)
 
-    pt_thermal = PTThermalCoeffs(K, ρCp, dt, di, li)
-    thermal_bc = (flux_x=false, flux_y=false)
-
-    @parallel (1:nx, 1:ny) init_T!(thermal.T, xci[2])
+    @parallel (1:nx, 1:ny) init_T!(thermal.T, xvi[2])
     @parallel assign!(thermal.Told, thermal.T)
+
+    dt = 0.5 / 4.1 * min(di...)^2 / κ
 
     t = 0.0
     it = 0
     nt = Int(ceil(ttot / dt))
     # Physical time loop
-    local iters
     while it < nt
-        iters = solve!(
+        solve!(
             thermal,
-            pt_thermal,
             thermal_parameters,
             thermal_bc,
-            ni,
             di,
-            dt;
-            iterMax=10e3,
-            nout=1,
-            verbose=false,
+            dt
         )
         it += 1
         t += dt
     end
 
-    return (ni=ni, xci=xci, li=li, di=di), thermal, iters
+    return (ni=ni, xci=xci, xvi=xvi, li=li, di=di), thermal.T
 end
