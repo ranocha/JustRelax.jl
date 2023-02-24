e9780bc3d68de63d31cc2732bae0b5ca1436b77a
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-02-24 15:47:24 +0100

    fix checkpointing

--- src/IO/DataIO.jl
+++ src/IO/DataIO.jl
@@ -29,8 +29,11 @@ function metadata(src, file, dst)
         mkpath(dest)
     end
     for f in (file, "Manifest.toml", "Project.toml")
-        cp(joinpath(src, f), dst)
+        newfile = joinpath(dst, basename(f))
+        isfile(newfile) && rm(newfile)
+        cp(joinpath(src, f), newfile)
     end
+    return nothing
 end
 
 end
