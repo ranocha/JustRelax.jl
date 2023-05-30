625e3cf8d928a12bcb6da65a342094c9fa0363b8
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-05-30 16:38:03 +0200

    improved indomain

--- src/particles/shuffle_vertex.jl
+++ src/particles/shuffle_vertex.jl
@@ -75,6 +75,7 @@ end
     if (i ≤ nx - 1) && (j ≤ ny - 1) && (k ≤ nz - 1)
         _shuffle_particles_vertex!(particle_coords, grid, dxi, nxi, index, (i, j, k), args)
     end
+
     return nothing
 end
 
@@ -226,15 +227,19 @@ function find_free_memory(index, I::Vararg{Int64, N}) where {N}
     return 0
 end
 
-@generated function indomain(p::NTuple{N,T}, domain_limits) where {N, T}
-    quote
-        Base.@_inline_meta
-        Base.Cartesian.@nexprs $N i -> (
-            @inbounds (domain_limits[i][1] > p[i]) && return false;
-            @inbounds (p[i] > domain_limits[i][2]) && return false
-        )
-        return true
-    end
+# @generated function indomain(p::NTuple{N,T}, domain_limits) where {N, T}
+#     quote
+#         Base.@_inline_meta
+#         Base.Cartesian.@nexprs $N i -> (
+#             @inbounds (domain_limits[i][1] > p[i]) && return false;
+#             @inbounds (p[i] > domain_limits[i][2]) && return false
+#         )
+#         return true
+#     end
+# end
+
+function indomain(p::NTuple{N,T}, domain_limits) where {N, T}
+    (p[1] ≥ domain_limits[1][1]) * (p[1] ≤ domain_limits[1][2]) * (p[2] ≥ domain_limits[2][1]) * (p[2] ≤ domain_limits[2][2])
 end
 
 @generated function indomain(idx_child::NTuple{N,Integer}, nxi::NTuple{N,Integer}) where {N}
@@ -296,7 +301,6 @@ function clean_particles!(particles::Particles, grid::NTuple{2,T}, args) where {
     # unpack
     (; coords, index) = particles
     
-    # px, py = particle_coords
     dxi = compute_dx(grid)
 
     @parallel (1:size(index, 1), 1:size(index, 2)) _clean!(
@@ -324,84 +328,3 @@ end
     end
     return
 end
-
-
-# function _shuffle_particles_vertex!(
-#     px, py, grid, dxi, nxi, index, icell, jcell, args::NTuple{N,T}
-# ) where {N,T}
-#     nx, ny = nxi
-
-#     # closures --------------------------------------
-#     @inline child_index(i, j) = (icell + i, jcell + j)
-#     @inline function cache_args(
-#         args::NTuple{N1,T}, ip, child::Vararg{Int64,N2}
-#     ) where {T,N1,N2}
-#         return ntuple(i -> args[i][ip, child...], Val(N1))
-#     end
-#     @inline function find_free_memory(icell, jcell)
-#         for i in axes(index, 1)
-#             @inbounds index[i, icell, jcell] == 0 && return i
-#         end
-#         return 0
-#     end
-#     # -----------------------------------------------
-
-#     # coordinate of the lower-most-left coordinate of the parent cell 
-#     corner_xi = corner_coordinate(grid, icell, jcell)
-#     # cell where we check for incoming particles
-#     parent = icell, jcell
-#     # iterate over neighbouring (child) cells
-#     for j in -1:1, i in -1:1
-#         ichild, jchild = child_index(i, j)
-#         # ignore parent cell
-#         @inbounds if parent != (ichild, jchild) &&
-#             (1 ≤ ichild ≤ nx - 1) &&
-#             (1 ≤ jchild ≤ ny - 1)
-
-#             # iterate over particles in child cell 
-#             for ip in axes(px, 1)
-#                 if index[ip, ichild, jchild] # true if memory allocation is filled with a particle
-#                     p_child = (px[ip, ichild, jchild], py[ip, ichild, jchild])
-
-#                     # check whether the incoming particle is inside the cell and move it
-#                     if isincell(p_child, corner_xi, dxi)
-
-#                         # hold particle variables
-#                         current_px = px[ip, ichild, jchild]
-#                         current_py = py[ip, ichild, jchild]
-#                         # cache out fields
-#                         current_args = cache_args(args, ip, ichild, jchild)
-
-#                         (isnan(current_px) || isnan(current_py)) && continue
-
-#                         # remove particle from child cell
-#                         index[ip, ichild, jchild] = false
-#                         px[ip, ichild, jchild] = NaN
-#                         py[ip, ichild, jchild] = NaN
-
-#                         for t in eachindex(args)
-#                             args[t][ip, ichild, jchild] = NaN
-#                         end
-
-#                         # check whether there's empty space in parent cell
-#                         free_idx = find_free_memory(icell, jcell)
-#                         free_idx == 0 && continue
-
-#                         # move it to the first free memory location
-#                         index[free_idx, icell, jcell] = true
-#                         px[free_idx, icell, jcell] = current_px
-#                         py[free_idx, icell, jcell] = current_py
-
-#                         # println("($ip, $icell, $jcell) moved to ($free_idx, $ichild, $jchild)")
-#                         # move fields in the memory
-#                         for t in eachindex(args)
-#                             args[t][free_idx, icell, jcell] = current_args[t]
-#                         end
-#                     end
-#                 end
-#             end
-#         end
-#     end
-
-#     return nothing
-# end
